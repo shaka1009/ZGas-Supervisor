f6d011981c5d96c1822f8212e7f6129160a6fa2d
Jesus Magallanes <shaka1009@live.com.mx>
2021-06-10 01:07:06 -0500

    V1 - Visor

--- app/src/main/java/zgas/supervisor/HomeVisor.java
+++ app/src/main/java/zgas/supervisor/HomeVisor.java
@@ -4,16 +4,32 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
+import com.firebase.geofire.GeoLocation;
+import com.firebase.geofire.GeoQueryEventListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.database.DatabaseError;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import zgas.supervisor.providers.AuthProvider;
+import zgas.supervisor.providers.GeofireProvider;
 
 public class HomeVisor extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private AuthProvider mAuthProvider;
+    private GeofireProvider mGeofireProvider;
+
+
+    private LatLng mCurrentLatLng;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +39,17 @@ public class HomeVisor extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        mAuthProvider = new AuthProvider();
+        mGeofireProvider = new GeofireProvider("active_drivers");
+
+
+
+        mCurrentLatLng = new LatLng(20.6340165, -103.3536772);
+
+        getActiveDrivers();
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -42,5 +58,66 @@ public class HomeVisor extends FragmentActivity implements OnMapReadyCallback {
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+
+
+    private List<Marker> mDriversMarkers = new ArrayList<>();
+
+    private void getActiveDrivers() {
+        mGeofireProvider.getActiveDrivers(mCurrentLatLng, 10).addGeoQueryEventListener(new GeoQueryEventListener() {
+            @Override
+            public void onKeyEntered(String key, GeoLocation location) {
+                // AÑADIREMOS LOS MARCADORES DE LOS CONDUCTORES QUE SE CONECTEN EN LA APLICACION
+
+                for (Marker marker: mDriversMarkers) {
+                    if (marker.getTag() != null) {
+                        if (marker.getTag().equals(key)) {
+                            return;
+                        }
+                    }
+                }
+
+                LatLng driverLatLng = new LatLng(location.latitude, location.longitude);
+                Marker marker = mMap.addMarker(new MarkerOptions().position(driverLatLng).title("Conductor disponible" + key).icon(BitmapDescriptorFactory.fromResource(R.drawable.icon_car)));
+                marker.setTag(key);
+                mDriversMarkers.add(marker);
+            }
+
+            @Override
+            public void onKeyExited(String key) {
+                for (Marker marker: mDriversMarkers) {
+                    if (marker.getTag() != null) {
+                        if (marker.getTag().equals(key)) {
+                            marker.remove();
+                            mDriversMarkers.remove(marker);
+                            return;
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onKeyMoved(String key, GeoLocation location) {
+                // ACTUALIZAR LA POSICION DE CADA CONDUCTOR
+                for (Marker marker: mDriversMarkers) {
+                    if (marker.getTag() != null) {
+                        if (marker.getTag().equals(key)) {
+                            marker.setPosition(new LatLng(location.latitude, location.longitude));
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onGeoQueryReady() {
+
+            }
+
+            @Override
+            public void onGeoQueryError(DatabaseError error) {
+
+            }
+        });
     }
 }
